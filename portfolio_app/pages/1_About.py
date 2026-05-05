a38622fee0b91643f042bd80893d0c98956aff9b
Samrat_Malla <samratmalla2006@gmail.com>
2026-05-05 15:07:07 +0545

    Update 1_About.py

--- portfolio_app/pages/1_About.py
+++ portfolio_app/pages/1_About.py
@@ -80,7 +80,7 @@ st.markdown("""
 st.set_page_config(page_title="Its About Me,", page_icon="👌")
 
 
-
+@st.cache_data
 def load_image_base64(path: str) -> str:
     """Load an image and return its Base64 string."""
     with open(path, "rb") as f:
